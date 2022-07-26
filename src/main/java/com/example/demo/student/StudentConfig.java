8cfe5215cab063b32fb9982cabff281ff556942e
agbajemm <agbajemhmd316@gmail.com>
2022-07-26 23:07:07 +0100

    repository

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student muhammed = new Student(
+                    "Muhammed",
+                    "muhammed@gmail.com",
+                    LocalDate.of(2000, Month.MARCH, 5),
+                    22
+            );
+
+            Student afolabi = new Student(
+                    "Afolabi",
+                    "afolabi@gmail.com",
+                    LocalDate.of(2000, Month.MARCH, 5),
+                    22
+            );
+
+            repository.saveAll(
+                    List.of(muhammed, afolabi)
+            );
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,7 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -9,15 +9,15 @@ import java.util.List;
 
 @Component
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Muhammed",
-                        "muhammed@gmail.com",
-                        LocalDate.of(2000, Month.MARCH, 5),
-                        22
-                )
-        );
+        return studentRepository.findAll();
     }
 }
